8c6a6ffdb3a1984fa01db05fd7f45e7dd8dbe771
JuanArango0508 <arangoj945@gmail.com>
2025-04-10 16:49:18 -0500

    Se finaliza ejercicio

--- src/App.java
+++ src/App.java
@@ -15,6 +15,7 @@ public class App {
         double auxilioTransporte = 200000;
         double cantidadHorasExtras = 0;
         double valorHoraExtra = 0;
+        double totalValorHorasExtras = 0; 
 
         System.out.println("Ingrese el nombre del empleado: ");
         nombreEmpleado = sc.nextLine();
@@ -22,6 +23,39 @@ public class App {
         documentoEmpleado = sc.nextLine();
         System.out.println("Ingrese el dia de descanso: ( Lunes - Viernes )");
         diaDescanso = sc.nextLine();
+        System.out.println("Ingrese el valor de la hora:" );
+        valorHora = sc.nextDouble();
+        System.out.println("Ingrese la cantidad de horas trabajadas:" );
+        horasTrabajadas = sc.nextDouble();
+
+        salarioBruto = horasTrabajadas * valorHora;
+        if (salarioBruto <= 2*valorSalarioMinimo) {
+            auxilioTransporte = 200000;
+            bonificacionEmpleado = salarioBruto*0.1;
+        } else {
+            auxilioTransporte = 0;
+            bonificacionEmpleado = 0; 
+        }
+        deduccionPension = salarioBruto*0.04;
+        deduccionSalud = salarioBruto*0.04;
+        if (horasTrabajadas > 96) {
+            cantidadHorasExtras = horasTrabajadas - 96;
+            valorHoraExtra = valorHora * 1.25;
+            totalValorHorasExtras = cantidadHorasExtras * valorHoraExtra; 
+        }
+        if (horasTrabajadas < 24) {
+            System.out.println("Aún no le puedo pagar; trabaje más");
+        } else {
+            salarioNeto = salarioBruto - deduccionPension - deduccionSalud + auxilioTransporte + bonificacionEmpleado + totalValorHorasExtras;
+            System.out.println("Nombre del empleado: " +nombreEmpleado);
+            System.out.println("Documento: " +documentoEmpleado);
+            System.out.println("Día de descanso: " + diaDescanso);
+            System.out.println("Salario bruto: " +salarioBruto);
+            System.out.println("El salario neto del empleado es: "+salarioNeto);
+            System.out.println("");
+
+        }
+            
         
     }
 }
